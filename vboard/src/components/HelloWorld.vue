356392b4de65e386d9c1f19f112a33a7c2f14c94
Tudor <stupariu.tudorf@gmail.com>
2017-11-29 15:58:16 +0200

    Creates Initial Vue project
    *Set-up the project
    *Set-up Vuetify with the project
    *Set-up SCSS support
    *Demo file separation

---
+++ vboard/src/components/HelloWorld.vue
@@ -0,0 +1,12 @@
+<template src="./template.html"></template>
+<style lang="scss" scoped src="./style.scss"></style>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+    }
+  }
+}
+</script>
