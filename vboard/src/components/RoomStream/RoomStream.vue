f98fe8c6ba6eeed0b9befd67f43c500b4bdea0ad
Tudor Stupariu <tudorstupariu@Tudors-MacBook-Air.local>
2018-02-22 15:15:01 +0200

    Added manually viewing a stream, non dependent on room for now

--- vboard/src/components/RoomStream/RoomStream.vue
+++ vboard/src/components/RoomStream/RoomStream.vue
@@ -4,6 +4,11 @@
 			<h1>Stream for room with id {{room_id}}</h1>
 		</div>
 		<canvas-stream />
+		<form>
+			<textarea id="incoming"></textarea>
+			<button type="submit">submit</button>
+		</form>
+		<pre id="outgoing"></pre>
 	</div>
 </template>
 
@@ -24,6 +29,35 @@ export default {
 	},
 	mounted() {
 		this.room_id = this.$route.params.room_id
+
+		var SimplePeer = require('simple-peer')
+
+		var canvas = document.querySelector("#canvas");
+		var myStream = canvas.captureStream(15);
+
+		var p = new SimplePeer({ initiator: true, trickle: false, stream: myStream})
+		
+		p.on('signal', function (data) {
+			console.log('SIGNAL', JSON.stringify(data))
+			document.querySelector('#outgoing').textContent = JSON.stringify(data)
+		})
+		
+		document.querySelector('form').addEventListener('submit', function (ev) {
+			ev.preventDefault()
+			p.signal(JSON.parse(document.querySelector('#incoming').value))
+		})
+		
+		p.on('connect', function () {
+			console.log('CONNECT')
+			p.send('whatever' + Math.random())
+			var ctx = canvas.getContext("2d");
+			ctx.fillStyle="#FF0000";
+			ctx.fillRect(100,100,100,100);
+		})
+		
+		p.on('data', function (data) {
+			console.log('data: ' + data)
+		})
 	}
 }
 </script>
--- vboard/src/components/RoomView/RoomView.vue
+++ vboard/src/components/RoomView/RoomView.vue
@@ -1,6 +1,14 @@
 <template>
 	<div>
 		<h1>View for room with id {{room_id}}</h1>
+		<div>
+			<video id="video" ref="video" controls autoplay style="height: 450px; width: 800px"></video>
+			<form>
+				<textarea id="incoming"></textarea>
+				<button type="submit">submit</button>
+			</form>
+			<pre id="outgoing"></pre>
+		</div>
 	</div>
 </template>
 
@@ -18,6 +26,42 @@ export default {
 	},
 	mounted() {
 		this.room_id = this.$route.params.room_id
+
+		var SimplePeer = require('simple-peer')
+		var p = new SimplePeer({ initiator: false, trickle: false })
+		
+		p.on('error', function (err) { console.log('error', err) })
+		
+		p.on('signal', function (data) {
+			console.log('SIGNAL', JSON.stringify(data))
+			document.querySelector('#outgoing').textContent = JSON.stringify(data)
+		})
+		
+		document.querySelector('form').addEventListener('submit', function (ev) {
+			ev.preventDefault()
+			p.signal(JSON.parse(document.querySelector('#incoming').value))
+		})
+		
+		p.on('connect', function () {
+			console.log('CONNECT')
+			p.send('whatever' + Math.random())
+			var video = document.querySelector('video')
+			video.srcObject = this.str
+			video.onloadedmetadata = function(e) {
+				video.play();
+			};
+			console.log(this.str)
+		})
+		
+		p.on('data', function (data) {
+			console.log('data: ' + data)
+		})
+
+		p.on('stream', function (stream) {
+				// var video = document.querySelector('video')
+				// video.srcObject = stream
+				this.str = stream
+			})
 	}
 }
 </script>
